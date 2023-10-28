52cdaede14d6d165832c724fbd41f4aa99219a68
Renato Rodrigues <spamcares-github@yahoo.com>
2023-10-28 18:03:32 -0300

    - atualização de documentação readme

--- scripts/schema/1-init.sql
+++ scripts/schema/1-init.sql
@@ -1,11 +1,11 @@
 -- Criação de banco de dados
--- CREATE DATABASE IF NOT EXISTS FAST_N_FOODIOUS;
+CREATE DATABASE IF NOT EXISTS FAST_N_FOODIOUS;
 
 -- Configuração de permissão para usuário da aplicação
--- GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, ALTER, DROP, INDEX, REFERENCES ON FAST_N_FOODIOUS.* TO 'fnf_user'@'%';
--- FLUSH PRIVILEGES;
+GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, ALTER, DROP, INDEX, REFERENCES ON FAST_N_FOODIOUS.* TO 'fnf_user'@'%';
+FLUSH PRIVILEGES;
 
--- USE FAST_N_FOODIOUS;
+USE FAST_N_FOODIOUS;
 
 --
 -- CRIAÇÃO DE TABELAS
@@ -13,68 +13,63 @@
 
 -- Tabela cliente
 CREATE TABLE IF NOT EXISTS CLIENTE (
-                                       ID INT PRIMARY KEY AUTO_INCREMENT,
+                                       ID INT AUTO_INCREMENT PRIMARY KEY,
                                        NOME VARCHAR(255) NOT NULL,
-                                       EMAIL VARCHAR(255) NOT NULL UNIQUE,
-                                       CPF VARCHAR(11) NOT NULL UNIQUE
+                                       EMAIL VARCHAR(255) NOT NULL,
+                                       CPF VARCHAR(11) NOT NULL
 );
 
 -- indexes para tabela CLIENTE
--- CREATE UNIQUE INDEX cliente_email_idx ON CLIENTE(EMAIL);
--- CREATE UNIQUE INDEX cliente_cpf_idx ON CLIENTE(CPF);
+CREATE UNIQUE INDEX cliente_email_idx ON CLIENTE(EMAIL);
+CREATE UNIQUE INDEX cliente_cpf_idx ON CLIENTE(CPF);
 
--- Tabela CATEGORIA_PRODUTO -- sem auto_increment porque o conteúdo da tabela é fixa
+-- Tabela CATEGORIA_PRODUTO
 CREATE TABLE IF NOT EXISTS CATEGORIA_PRODUTO (
-                                                 ID INT PRIMARY KEY, 
-                                                 NOME VARCHAR(255) NOT NULL UNIQUE
+                                                 ID INT PRIMARY KEY, -- sem auto_increment porque o conteúdo da tabela é fixa
+                                                 NOME VARCHAR(255) NOT NULL
 );
 
 -- indexes para tabela CATEGORIA_PRODUTO
--- CREATE UNIQUE INDEX categoria_produto_nome_idx ON CATEGORIA_PRODUTO(NOME);
+CREATE UNIQUE INDEX categoria_produto_nome_idx ON CATEGORIA_PRODUTO(NOME);
 
 -- Tabela PRODUTO
 CREATE TABLE IF NOT EXISTS PRODUTO (
-                                       ID INT PRIMARY KEY AUTO_INCREMENT,
-                                       PRODUTO_CATEGORIA_ID INT NOT NULL,
-                                       NOME VARCHAR(255) NOT NULL UNIQUE,
+                                       ID INT AUTO_INCREMENT PRIMARY KEY,
+                                       PRODUTO_CATEGORIA_ID INT NOT NULL, CONSTRAINT FK_PRODUTO_CATEGORIA FOREIGN KEY (PRODUTO_CATEGORIA_ID) REFERENCES CATEGORIA_PRODUTO(ID),
+                                       NOME VARCHAR(255) NOT NULL,
                                        DESCRICAO VARCHAR(255) NOT NULL,
                                        PRECO DECIMAL(5,2) NOT NULL,
-                                       IMAGEM TEXT,
-                                       ATIVO BOOLEAN NOT NULL DEFAULT TRUE,
-                                       FOREIGN KEY (PRODUTO_CATEGORIA_ID) REFERENCES CATEGORIA_PRODUTO(ID)
+                                       IMAGEM TEXT, -- base64
+                                       ATIVO BOOLEAN NOT NULL DEFAULT TRUE
 );
 
 -- indexes para tabela PRODUTO
--- CREATE UNIQUE produto_nome_idx ON PRODUTO(NOME);
+CREATE UNIQUE INDEX produto_nome_idx ON PRODUTO(NOME);
 
 -- Tabela PEDIDO
 CREATE TABLE IF NOT EXISTS PEDIDO (
-                                       ID INT PRIMARY KEY AUTO_INCREMENT,
-                                       PEDIDO_CLIENTE_ID INT NOT NULL,
+                                       ID INT AUTO_INCREMENT PRIMARY KEY,
+                                       PEDIDO_CLIENTE_ID INT NOT NULL, CONSTRAINT FK_PEDIDO_CLIENTE_ID FOREIGN KEY (PEDIDO_CLIENTE_ID) REFERENCES CLIENTE(ID),
                                        DATA_INICIO VARCHAR(255) NOT NULL,
                                        ESTADO_PEDIDO INT NOT NULL,
                                        ATIVO BOOLEAN NOT NULL DEFAULT TRUE,
-                                       TOTAL DECIMAL(8,2) NULL,
-                                       FOREIGN KEY (PEDIDO_CLIENTE_ID) REFERENCES CLIENTE(ID)
+                                       TOTAL DECIMAL(8,2) NULL
 );
 
 -- Tabela ITEMS DE PEDIDO
 CREATE TABLE IF NOT EXISTS ITEM_PEDIDO (
-    ID INT PRIMARY KEY AUTO_INCREMENT,
-    PEDIDO_ID INT NOT NULL,
-    PRODUTO_ID INT NOT NULL,
-    QUANTIDADE INT NOT NULL,
-    FOREIGN KEY (PEDIDO_ID) REFERENCES PEDIDO(ID),
-    FOREIGN KEY (PRODUTO_ID) REFERENCES PRODUTO(ID)
+                                       ID INT AUTO_INCREMENT PRIMARY KEY,
+                                       PEDIDO_ID INT NOT NULL, CONSTRAINT FK_PEDIDO_ID FOREIGN KEY (PEDIDO_ID) REFERENCES PEDIDO(ID),
+                                       PRODUTO_ID INT NOT NULL, CONSTRAINT FK_PRODUTO_ID FOREIGN KEY (PRODUTO_ID) REFERENCES PRODUTO(ID),
+                                       QUANTIDADE INT NOT NULL
 );
 
 -- Tabela PAGAMENTO
 CREATE TABLE IF NOT EXISTS PAGAMENTO (
-                                       ID INT PRIMARY KEY AUTO_INCREMENT,
-                                       PEDIDO_ID INT NOT NULL, 
+                                       ID INT AUTO_INCREMENT PRIMARY KEY,
+                                       PEDIDO_ID INT NOT NULL, CONSTRAINT FK_PAGAMENTO_PEDIDO_ID FOREIGN KEY (PEDIDO_ID) REFERENCES PEDIDO(ID),
                                        TRANSACAO_ID VARCHAR(255) NOT NULL,
                                        ESTADO_PAGAMENTO INT NOT NULL,
                                        TOTAL DECIMAL(8,2) NOT NULL,
-                                       DATA_HORA_PAGAMENTO DATETIME NULL,
-                                       FOREIGN KEY (PEDIDO_ID) REFERENCES PEDIDO(ID)
+                                       DATA_HORA_PAGAMENTO DATETIME NULL
 );
--- src/presentation/swagger/application.constants.ts
+++ src/presentation/swagger/application.constants.ts
@@ -2,9 +2,9 @@ export class SwaggerConstants {
    //Swaggwer
    public static readonly SWAGGER_PATH: string = 'api';
    public static readonly SWAGGER_TITLE: string = '🍔 Fast & Foodious API';
-   public static readonly SWAGGER_VERSION: string = '2.1.0';
+   public static readonly SWAGGER_VERSION: string = '3.0.0';
    public static readonly SWAGGER_DESCRIPTION: string =
-      'Sistema de auto-atendimento de fast food. Projeto de conclusão da Fase 02 da pós gradução em Software Architecture.';
+      'Sistema de auto-atendimento de fast food. Projeto de conclusão da Fase 03 da pós gradução em Software Architecture.';
    public static readonly SWAGGER_EXTERNAL_DOC_DESCRIPTION: string = '🔗 Github Fast & Foodious';
    public static readonly SWAGGER_EXTERNAL_DOC_URL: string = 'https://github.com/rodrigo-ottero/fast-n-foodious';
 }
