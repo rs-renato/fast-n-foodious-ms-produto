388a02700927d363270a5e99dd8347884560d26b
Felipe Rosa <felipemax.suporteti@gmail.com>
2024-01-07 20:03:48 +0000

    style: lint format

--- src/presentation/rest/health/api/health.api.spec.ts
+++ src/presentation/rest/health/api/health.api.spec.ts
@@ -2,25 +2,25 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { HealthRestApi } from 'src/presentation/rest/health/api/health.api';
 
 describe('HealthRestApi', () => {
-   let restApi: HealthRestApi;
+  let restApi: HealthRestApi;
 
-   beforeEach(async () => {
-      // Configuração do módulo de teste
-      const module: TestingModule = await Test.createTestingModule({
-         controllers: [HealthRestApi],
-      }).compile();
+  beforeEach(async () => {
+    // Configuração do módulo de teste
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [HealthRestApi],
+    }).compile();
 
-      // Desabilita a saída de log
-      module.useLogger(false);
+    // Desabilita a saída de log
+    module.useLogger(false);
 
-      // Obtém a instância do restApi e do serviço a partir do módulo de teste
-      restApi = module.get<HealthRestApi>(HealthRestApi);
-   });
+    // Obtém a instância do restApi e do serviço a partir do módulo de teste
+    restApi = module.get<HealthRestApi>(HealthRestApi);
+  });
 
-   describe('health check', () => {
-      it('deve retormnar up', async () => {
-         const result = await restApi.check();
-         expect(result.status).toEqual('OK');
-      });
-   });
+  describe('health check', () => {
+    it('deve retormnar up', async () => {
+      const result = await restApi.check();
+      expect(result.status).toEqual('OK');
+    });
+  });
 });
