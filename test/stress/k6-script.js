cad2a5e748636d87cfd6a90815e7b79f310aa0c5
Renato Rodrigues <spamcares-github@yahoo.com>
2024-01-10 12:29:17 -0300

    - Atualização de documentação - Ajustes de processo morto (local) na execução bdd - Ajustes de console output bdd

--- test/stress/k6-script.js
+++ test/stress/k6-script.js
@@ -2,5 +2,5 @@ import http from 'k6/http';
 
 export default function () {
   let fnfSvc = __ENV.FAST_N_FOODIOUS_SVC || '127.0.0.1';
-  http.get(`http://${fnfSvc}/v1/categoria`);
+  http.get(`http://${fnfSvc}/health`);
 }
