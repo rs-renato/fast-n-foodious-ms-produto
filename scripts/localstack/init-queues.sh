9de255d8543bd401b591bb5a0af43a9f14ca5aa4
Renato Rodrigues <spamcares-github@yahoo.com>
2024-02-29 13:36:37 -0300

    - ajustes de email identity

--- scripts/localstack/init-queues.sh
+++ scripts/localstack/init-queues.sh
@@ -4,4 +4,4 @@ awslocal sqs create-queue --endpoint-url http://localhost:4566 --queue-name webh
 awslocal sqs create-queue --endpoint-url http://localhost:4566 --queue-name solicitar-pagamento-req.fifo --attributes "FifoQueue=true"
 awslocal sqs create-queue --endpoint-url http://localhost:4566 --queue-name preparacao-pedido-req.fifo --attributes "FifoQueue=true"
 awslocal sqs list-queues --endpoint-url http://localhost:4566
-awslocal ses verify-email-identity --email-address sac@fnf.com --endpoint-url=http://localhost:4566
+awslocal ses verify-email-identity --email-address sac.fast.n.foodious@gmail.com --endpoint-url=http://localhost:4566
