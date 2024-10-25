a1a7e10886cdbdb2118284fb96b00310586e2889
Renato Rodrigues <spamcares-github@yahoo.com>
2024-10-25 11:50:18 -0300

    Repo update

--- src/presentation/swagger/application.constants.ts
+++ src/presentation/swagger/application.constants.ts
@@ -6,6 +6,5 @@ export class SwaggerConstants {
   public static readonly SWAGGER_DESCRIPTION: string =
     'Sistema de auto-atendimento de fast food. Projeto de conclusão da Fase 05 da pós gradução em Software Architecture.';
   public static readonly SWAGGER_EXTERNAL_DOC_DESCRIPTION: string = '🔗 Github Fast & Foodious - Produto';
-  public static readonly SWAGGER_EXTERNAL_DOC_URL: string =
-    'https://github.com/rs-renato/fast-n-foodious-ms-produto';
+  public static readonly SWAGGER_EXTERNAL_DOC_URL: string = 'https://github.com/rs-renato/fast-n-foodious-ms-produto';
 }
